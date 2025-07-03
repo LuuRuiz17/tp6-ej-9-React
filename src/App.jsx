a0f0c8f999d3b39b4993cdc640d0d9c8080ca189
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-07-03 01:06:48 -0300

    Arregle el img del perrito

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FormularioCitas from './components/FormularioCitas';
+import perritoIcon from './assets/perrito-icon.png';
 
 function App() {
 
   return (
     <main className='container my-4 d-flex flex-column'>
-    <h1 className='color-loop text-center'>Administrador Pacientes de Veterinaria <img className='mb-3' src="../public/perrito-icon.png" alt='Ícono de perro'></img></h1>
+    <h1 className='color-loop text-center'>Administrador Pacientes de Veterinaria <img className='mb-3' src={perritoIcon} alt='Ícono de perro'></img></h1>
     <FormularioCitas></FormularioCitas>
     </main>
   )
